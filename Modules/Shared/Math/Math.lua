7f92ba3a67bb88628e7aaf903e73f74e5e2306dd
Ew-Developer <74792428+Ew-Developer@users.noreply.github.com>
2020-12-23 14:37:08 +0100

    Update Math.lua

--- Modules/Shared/Math/Math.lua
+++ Modules/Shared/Math/Math.lua
@@ -1,11 +1,18 @@
-local Module = {}
+local Math = {}
 
-function Module.inverse(Num)
+function Math.inverse(Num)
   Num = tonumber(Num)
   return Num - (Num * 2)
 end
 
-setmetatable(Module,{
+function Math.huge()
+  return math.huge
+end
+function Math.low()
+  return -(Math.huge())
+end
+
+setmetatable(Math,{
   __index = math
 })
-return Module
+return Math
