f9ff30423a4e33532e21930c12ddbd17d325ac64
Ew-Developer <74792428+Ew-Developer@users.noreply.github.com>
2020-12-23 14:48:09 +0100

    Update IKService.lua

--- Modules/Shared/IKService/IKService.lua
+++ Modules/Shared/IKService/IKService.lua
@@ -4,22 +4,21 @@ local EwModules = require(game:GetService("ReplicatedStorage"):WaitForChild("EwM
 local math = EwModules("Math")
 
 function IK.solve(Origin,Target,Len1,Len2)
-  local Localized = Origin:pointToObjectSpace(Target)
+	local Localized = Origin:pointToObjectSpace(Target)
 	local LocalizedUnit = Localized.Unit
 	local Len3 = Localized.Magnitude
-  
-  local Axis = Vector3.new(0,0,-1):Cross(LocalizedUnit)
+	
+	local Axis = Vector3.new(0,0,-1):Cross(LocalizedUnit)
 	local Angle = math.acos(-LocalizedUnit.Z)
 	local Plane = Origin * CFrame.fromAxisAngle(Axis, Angle)
-  
-  if Len3 < math.max(Len2,Len1) - math.min(Len2,Len1) then
+	if Len3 < math.max(Len2,Len1) - math.min(Len2,Len1) then
 		return Plane * CFrame.new(0,0, math.max(Len2,Len1) - math.min(Len2,Len1) - Len3), -math.pi/2, math.pi
-  elseif Len3 > Len1 + Len2 then
+	elseif Len3 > Len1 + Len2 then
 		return Plane * CFrame.new(0,0,Len1 + Len2 - Len3),math.pi/2,0
-  else
+	else
 		local Angle1 = -math.acos((-(Len2 * Len2) + (Len1 * Len1) + (Len3 * Len3)) / (2 * Len1 * Len3))
 		local Angle2 = math.acos(((Len2  * Len2) - (Len1 * Len1) + (Len3 * Len3)) / (2 * Len2 * Len3))
-
+		
 		return Plane,Angle1 + math.pi/2,Angle2 - Angle1
 	end
 end
